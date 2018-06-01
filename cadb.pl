4291176dda6928df66ffe2a6f88fbb9460a618fc
Gabriel Smith <gabrielsmith@air.local>
2018-06-01 13:48:23 -0300

    added facts about the provinces of Canada and it's capitals

---
+++ cadb.pl
@@ -0,0 +1,29 @@
+country(canada).
+
+located(canada, north_america).
+
+province(ontario).
+province(quebec).
+province(nova_scotia).
+province(new_brunswick).
+province(manitoba).
+province(british_columbia).
+province(prince_edward_island).
+province(saskatchewan).
+province(alberta).
+province(newfoundland_and_labrador).
+
+capital(canada, ottawa).
+capital(ontario, toronto).
+capital(quebec, quebec_city).
+capital(nova_scotia, halifax).
+capital(new_brunswick, fredericton).
+capital(manitoba, winnipeg).
+capital(british_columbia, victoria).
+capital(prince_edward_island, charlottetown).
+capital(saskatchewan, regina).
+capital(alberta, edmonton).
+capital(newfoundland_and_labrador, st_johns).
+
+
+
