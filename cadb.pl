656b9463c1fd2af952f2f8d4540e31a7a091d562
EdLoivos <eduardo_loivos@id.uff.br>
2018-06-06 18:29:48 -0300

    10 questions

--- cadb.pl
+++ cadb.pl
@@ -25,7 +25,6 @@ capital('saskatchewan', 'regina').
 capital('alberta', 'edmonton').
 capital('newfoundland and labrador', 'st. john\'s').
 
-% attributes are (city, height, max temperature, min temperature)
 data('ottawa', 114, 11.3, 1.4).
 data('toronto', 173, 13.0, 3.3).
 data('quebec city', 74, 9.2, -0.8).
@@ -42,6 +41,8 @@ data('moncton', 71, 10.7, 0.1).
 data('vancouver', 4, 13.9, 6.8).
 data('saskatoon', 504, 8.6, -3.5).
 data('calgary', 1084, 10.8, -1.9).
+
+
 
 largest_city(canada, toronto).
 largest_city(ontario, toronto).
@@ -70,6 +71,11 @@ beach('british columbia','pacific').
 beach('alberta','none').
 beach('saskatchewan','none').
 
+ocean_exit(X) :-
+  ocean(X),
+  beach(Y, X),
+  format('The province of ~w has access to the ~w ocean', [Y, X]).
+
 province_capitals(X) :-
     province(X),
     capital(X, Y),
@@ -79,11 +85,6 @@ capital_is_largest_city(X) :-
     province(X),
     capital(X, Y),
     largest_city(X, Y).
-
-ocean_exit(X) :-
-    ocean(X),
-    beach(Y, X),
-    format('The province of ~w has access to the ~w ocean', [Y, X]).
 
 which_ocean(X) :-
     province(X),
@@ -113,18 +114,6 @@ whatis(X) :-
     format('~w is the largest city in ~w', [X, Y]).
 
 % -------------------------------
-
-max_height :-
-    findall(X, data(_, X, _, _), L),
-    max_list(L, V),
-    format('The maximum height is ~D', [V]).
-
-city_max_height :-
-    findall(X, data(_, X, _, _), L),
-    max_list(L, V),
-    data(C, V, _,_),
-    format('The highest city in Canada is ~w', [C]).
-
 height(X) :-
 	data(X,Y,_,_),
 	format('The height of ~w is ~d', [X,Y]).
@@ -132,4 +121,47 @@ height(X) :-
 weather(X) :-
 	data(X,_,Y,Z),
 	K is ((Y + Z)/2),
-	format('The average temperature of ~w is ~f °C', [X,K]).
+	format('The average temperature of ~w is ~f °C',[X,K]).
+	
+highest() :-
+	data(MaxC, MaxH, _,_),
+	\+ (data(C, H, _,_), C \= MaxC, MaxH < H),
+	format('The highest city is ~w'[MaxC]).
+	
+highestCaptal() :-
+	data(MaxC, MaxH, _,_),
+	\+ (data(C, H, _,_), C \= MaxC , capital(_,C), MaxH < H),
+	format('The highest city is ~w'[MaxC]).
+	
+compareTemp(X,Y) :-
+	data(X,_,K,Z),
+	K is ((K + Z)/2),
+	data(X,_,W,Z),
+	W is max(((W + Z)/2), K),
+	data(C,W,_,_),
+	format('~w is warmer'[C]).
+	
+greaterThanN(N) :-
+	format('The cities with temperature higher than ~d are: \n'[N]),
+	\+ (data(C,_, Min,Max), (Max+Min)/2 > N, write(C),nl).
+	
+
+allCap_BigC() :-
+	format('the provinces that it\'s capital is the biggest city are \n'),
+	\+ (capital(X,Y), province(X),largest_city(X,Y),write(X),nl ).
+	
+group_Ocean(O) :-
+	format('The provinces with access to ~w ocean are:'),
+	\+ (ocean_exit(X,O), write(X)).
+
+% ---------------------------------
+X:' is the captal of ':Y:'?' = capital(Y,X).
+'What is ':X:'?'= whatis(X).
+'What is the captal of ':X:'?' = province_capitals(X).
+'Which is the highest city?' = highest().
+'Which is the highest capital?' = highestCaptal().
+'Which provinces have capitals that are also your biggest city?' = allCap_BigC().
+'Which of the ':X:', ':Y:' is the warmer?'= compareTemp(X,Y).
+'Which city has an annual average greater than ':N:' degrees?'= greaterThanN(N).
+'What is the average temperature of ':X:'?'=weather(X).
+'Which provinces has access to ':X:' ocean?'= group_Ocean(X).
